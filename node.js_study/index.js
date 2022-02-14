4c6d303d05c5e582ae1970250440f5089db49a6a
GoWoong <popgun10@naver.com>
2022-02-14 12:48:48 +0900

    java script code test

--- node.js_study/index.js
+++ node.js_study/index.js
@@ -1,7 +1,58 @@
-const http = require('http');
+// const http = require('http');
 
-http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-}).listen(7777);
+// http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// }).listen(7777);
+
+const start = Date.now();
+
+function sayHi() {   
+  console.log( "Hello" );
+}
+
+let func = sayHi;    
+
+func(); 
+sayHi(); 
+
+let map = new Map();
+
+map.set('1', 'str1');   // 문자형 키
+map.set(1, 'num1');     // 숫자형 키
+map.set(true, 'bool1'); // 불린형 키
+
+// 객체는 키를 문자형으로 변환한다는 걸 기억하고 계신가요?
+// 맵은 키의 타입을 변환시키지 않고 그대로 유지합니다. 따라서 아래의 코드는 출력되는 값이 다릅니다.
+console.log( map.get(1)   ); // 'num1'
+console.log( map.get('1') ); // 'str1'
+console.log( map.size ); // 3
+console.log(map);
+
+let john = { name: "John" };
+
+// 고객의 가게 방문 횟수를 세본다고 가정해 봅시다.
+let visitsCountMap = new Map();
+
+// john을 맵의 키로 사용하겠습니다.
+visitsCountMap.set(john, 123);
+
+console.log( visitsCountMap.get(john) ); // 123
+console.log(visitsCountMap);
+
+const end = Date.now();
+
+
+console.log('소요된 시간은');
+console.log(`${end-start}ms`);
+
+function pow(x, n) {
+  if (n == 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
+
+console.log( pow(2, 3) ); // 8
